f1fc6b07b3c392af2304c9ed5fef42e86f467c17
CelineTran <Celinetran7737@yahoo.com>
2019-03-30 12:55:04 -0700

    Update template_index.js

--- template_index.js
+++ template_index.js
@@ -4,7 +4,7 @@ const functions = require('firebase-functions');
 const {WebhookClient} = require('dialogflow-fulfillment');
 const {Card, Suggestion} = require('dialogflow-fulfillment');
  
-process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+process.env.DEBUG = 'dialogflow:debug'; 
  
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
   const agent = new WebhookClient({ request, response });
