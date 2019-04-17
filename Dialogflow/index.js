6ee4a34ea5bbbdc0bf5c27931d61eb3bcbbc542b
CelineTran <Celinetran7737@yahoo.com>
2019-04-17 15:25:31 -0700

    Update index.js

--- Dialogflow/index.js
+++ Dialogflow/index.js
@@ -3,16 +3,16 @@ const {dialogflow} = require('actions-on-google');
 
 const app = dialogflow(); 
 
-app.intent('Default Welcome Intent', conv =>{
+app.intent('Default Welcome Intent', (conv) =>{
 	conv.ask("Hello World"); 
 });
 
-app.intent('My First Intent', conv =>{
+app.intent('My First Intent', (conv) =>{
 	var speechOutput = "This is my very first intent"; 
 	conv.ask(speechOutput); 
 }); 
 
-app.intent('Default Fallback Intent', conv =>{
+app.intent('Default Fallback Intent', (conv) =>{
 	conv.ask("I didn't get that"); 
 	conv.ask("I'm sorry, can you try again?"); 
 }); 
